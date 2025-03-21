622c60412479cc5d6c8b4f7916d770f9e54d668e
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-03-21 19:36:26 +0900

    edit build-system to fix bug

--- script/build-system.sh
+++ script/build-system.sh
@@ -59,7 +59,7 @@ snap install firefox chromium-browser -y
 
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
+sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge.list'
 rm microsoft.gpg
 apt update
 apt install microsoft-edge-stable -y
