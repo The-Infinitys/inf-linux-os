192ccfe4298ac0d76f580c8125237b96b6e3673f
The Infinity's <137460810+The-Infinitys@users.noreply.github.com>
2025-03-21 08:16:57 +0000

    update permission

--- script/config-rootfs.sh
+++ script/config-rootfs.sh
@@ -2,11 +2,10 @@
 #!/bin/bash
 
 if [ "$1" == "before" ]; then
+  cp -f src/ubuntu-oracular-full.list rootfs/etc/apt/sources.list
   cp script/config-chroot.sh rootfs/config-chroot.sh
   cp script/build-system.sh rootfs/build-system.sh
 elif [ "$1" == "after" ]; then
   rm -f rootfs/config-chroot.sh
   rm -f rootfs/build-system.sh
-else
-  cp -f src/ubuntu-oracular-full.list rootfs/etc/apt/sources.list
 fi
