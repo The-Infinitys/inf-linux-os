0b673f4e403e0ef089130a79f25c837aaa50bc9c
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-03-22 17:00:50 +0900

    build: refactor build scripts and update package installation process

--- script/build-rootfs.sh
+++ script/build-rootfs.sh
@@ -10,8 +10,45 @@ fi
 
 mkdir rootfs
 
-mmdebstrap --components="main restricted universe multiverse" \
+TARGET_PACKAGE=" \
+    open-infrastructure-system-boot \
+    open-infrastructure-system-build \
+    open-infrastructure-system-config \
+    open-infrastructure-system-images \
+    isolinux syslinux \
+    linux-headers-generic \
+    linux-image-generic \
+    kubuntu-desktop ubuntu-server \
+    build-essential curl vim \
+    open-vm-tools-desktop \
+    clamav \
+    bind9 \
+    openssh-server \
+    samba smbclient cifs-utils \
+    isc-dhcp-server \
+    minidlna \
+    language-pack-ja \
+    language-pack-kde-ja \
+    fonts-noto \
+    fcitx5-mozc mozc-utils-gui \
+    libreoffice-l10n-ja libreoffice-help-ja \
+    thunderbird-locale-ja \
+    systemd-sysv \
+    dbus \
+    iproute2 \
+    iputils-ping \
+    net-tools \
+    openssh-server \
+    apt-utils \
+    apt-transport-https \
+    wget \
+    nano \
+    sudo \
+    less \
+    locales \
+    mount \
+" mmdebstrap --components="main restricted universe multiverse" \
   oracular rootfs http://jp.archive.ubuntu.com/ubuntu/ --mode=auto\
-  --arch=amd64 --include=systemd-sysv,dbus,iproute2,iputils-ping,net-tools,openssh-server,apt-utils,apt-transport-https,wget,nano,sudo,less,locales,mount
+  --arch=amd64
 
 echo "Done."
--- script/config-rootfs.sh
+++ script/config-rootfs.sh
@@ -3,12 +3,12 @@
 if [ "$1" == "before" ]; then
   cp -f src/ubuntu-oracular-full.list rootfs/etc/apt/sources.list
   cp script/config-chroot.sh rootfs/config-chroot.sh
-  cp script/build-system.sh rootfs/build-system.sh
+  cp src/build-system.sh rootfs/build-system.sh
 elif [ "$1" == "after" ]; then
   rm -f rootfs/*.sh
 elif [ "$1" == "debug" ]; then
   cp script/config-chroot.sh rootfs/config-chroot.sh
-  cp script/debug-system.sh rootfs/debug-system.sh
+  cp src/debug-system.sh rootfs/debug-system.sh
 else
   echo "Usage: $0 {--before|--after}"
   exit 1
