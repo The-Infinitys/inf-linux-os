50725658d4fd84eaaab3cee2bd5ad0bc03344f08
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-03-22 18:19:56 +0900

    add echo system

--- package-infinity-linux.sh
+++ package-infinity-linux.sh
@@ -14,6 +14,7 @@ fi
 cd "$(dirname "$0")"
 
 # Create package directory
+echo "Creating package directory..."
 if [ -d "package" ]; then
   rm -rf package
 fi
@@ -22,25 +23,32 @@ mkdir package
 cd package
 
 # Create a 8GB empty image file
+echo "Creating a 8GB empty image file..."
 dd if=/dev/zero of=infinity-linux.img bs=1M count=8192
 
 # Initialize the image with GPT partition table
+echo "Initializing the image with GPT partition table..."
 parted infinity-linux.img --script mklabel gpt
 
 # Create bios-part partition (2MB, unformatted)
+echo "Creating bios-part partition (2MB, unformatted)..."
 parted infinity-linux.img --script mkpart bios-part 1MiB 3MiB
 
 # Create uefi-part partition (512MB, FAT32)
+echo "Creating uefi-part partition (512MB, FAT32)..."
 parted infinity-linux.img --script mkpart uefi-part fat32 3MiB 515MiB
 parted infinity-linux.img --script set 2 esp on
 
 # Create linux-boot partition (2048MB, ext4)
+echo "Creating linux-boot partition (2048MB, ext4)..."
 parted infinity-linux.img --script mkpart linux-boot ext4 515MiB 2563MiB
 
 # Create inf-linux partition (remaining space, ext4)
+echo "Creating inf-linux partition (remaining space, ext4)..."
 parted infinity-linux.img --script mkpart inf-linux ext4 2563MiB 100%
 
 # Format the partitions
+echo "Formatting the partitions..."
 LOOP_DEVICE=$(losetup --find --show infinity-linux.img)
 mkfs.vfat -F 32 ${LOOP_DEVICE}p2 -n uefi-part
 mkfs.ext4 ${LOOP_DEVICE}p3 -L linux-boot
@@ -48,35 +56,48 @@ mkfs.ext4 ${LOOP_DEVICE}p4 -L inf-linux
 losetup -d ${LOOP_DEVICE}
 
 # Mount the partitions
+echo "Mounting the partitions..."
 mkdir -p /mnt/linux-boot /mnt/inf-linux
 mount ${LOOP_DEVICE}p3 /mnt/linux-boot
 mount ${LOOP_DEVICE}p4 /mnt/inf-linux
 
 # Copy rootfs/boot to linux-boot
+echo "Copying rootfs/boot to linux-boot..."
 cp -r rootfs/boot/* /mnt/linux-boot/
 
 # Copy the rest of rootfs to inf-linux
+echo "Copying the rest of rootfs to inf-linux..."
 cp -r rootfs/* /mnt/inf-linux/
 rm -rf /mnt/inf-linux/boot
 
 # Unmount the partitions
+echo "Unmounting the partitions..."
 umount /mnt/linux-boot
 umount /mnt/inf-linux
 rmdir /mnt/linux-boot /mnt/inf-linux
 
 # Mount all partitions again for grub-install
+echo "Mounting all partitions again for grub-install..."
 mount ${LOOP_DEVICE}p3 /mnt/inf-linux/boot
 mount ${LOOP_DEVICE}p4 /mnt/inf-linux/
 mount ${LOOP_DEVICE}p2 /mnt/inf-linux/efi
 
+echo "Initializing chroot environment..."
 script/init-chroot.sh /mnt/inf-linux
+
 # Install GRUB
+echo "Installing GRUB..."
 chroot /mnt/inf-linux "grub-install --target=x86_64-efi --efi-directory=/efi --boot-directory=/boot ${LOOP_DEVICE} \
 grub-install --target=i386-pc --boot-directory=/boot ${LOOP_DEVICE}"
+
+echo "Exiting chroot environment..."
 script/exit-chroot.sh /mnt/inf-linux
 
 # Unmount the partitions
+echo "Unmounting the partitions..."
 umount /mnt/inf-linux/efi
 umount /mnt/inf-linux/boot
 umount /mnt/inf-linux
 rmdir /mnt/linux-boot /mnt/inf-linux
+
+echo "Script completed successfully."
