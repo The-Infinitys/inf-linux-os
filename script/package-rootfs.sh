7ccd1876102c5bfecbb12af68183a1b54ac36518
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-03-22 22:02:21 +0900

    build: enhance ISO creation process and add MD5/SHA512 checksum generation

--- script/package-rootfs.sh
+++ script/package-rootfs.sh
@@ -41,7 +41,20 @@ rm extracted/casper/filesystem.squashfs.gpg
 mksquashfs ../rootfs extracted/casper/filesystem.squashfs -comp xz
 printf $(du -sx --block-size=1 edit | cut -f1) > extracted/casper/filesystem.size
 
-gpg --local-user 2551D59A01F3022FAFB75644F440B26DF14188A2 --output extracted/casper/filesystem.squashfs.gpg --detach-sign extracted/casper/filesystem.squashfs
-
 printf $(du -sx --block-size=1 edit | cut -f1) > extracted/casper/filesystem.size
 
+cd extracted
+sudo rm md5sum.txt
+sudo find -type f -print0 | xargs -0 md5sum | grep -v isolinux/boot.cat | tee md5sum.txt
+cd ..
+
+# Export ISO
+
+sudo dd bs=1 count=446 if=kubuntu-24.10-desktop-amd64.iso of=mbr.img
+sudo dd bs=512 count=10128 skip=9127804 if=kubuntu-24.10-desktop-amd64.iso of=EFI.img
+sudo xorriso -indev "kubuntu-24.10-desktop-amd64.iso" -report_el_torito cmd
+
+sudo xorriso -outdev InfinityLinux-24.iso -map extracted / -- -volid "Infinity Linux OS Oracular" -boot_image grub grub2_mbr=mbr.img -boot_image any partition_table=on -boot_image any partition_cyl_align=off -boot_image any partition_offset=16 -boot_image any mbr_force_bootable=on -append_partition 2 28732ac11ff8d211ba4b00a0c93ec93b EFI.img -boot_image any appended_part_as=gpt -boot_image any iso_mbr_part_type=a2a0d0ebe5b9334487c068b6b72699c7 -boot_image any cat_path='/boot.catalog' -boot_image grub bin_path='/boot/grub/i386-pc/eltorito.img' -boot_image any platform_id=0x00 -boot_image any emul_type=no_emulation -boot_image any load_size=2048 -boot_image any boot_info_table=on -boot_image grub grub2_boot_info=on -boot_image any next -boot_image any efi_path=--interval:appended_partition_2:all:: -boot_image any platform_id=0xef -boot_image any emul_type=no_emulation -boot_image any load_size=4349952
+
+
+sha512sum InfinityLinux-24.iso > InfinityLinux-24.iso.sha512
